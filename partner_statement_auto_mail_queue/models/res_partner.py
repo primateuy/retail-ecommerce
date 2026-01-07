760abca4e7907fd68f1ed30573599b0cbae7c008
dyturralde <darylyturraldelopez@gmail.com>
2026-01-07 12:26:59 -0300

    [FIX] Arreglo de estado cuentas y pendiente

--- partner_statement_auto_mail_queue/models/res_partner.py
+++ partner_statement_auto_mail_queue/models/res_partner.py
@@ -2,8 +2,9 @@ import json
 import base64
 import logging
 
-from datetime import timedelta
+from datetime import timedelta, date
 from odoo import models, fields, api
+from dateutil.relativedelta import relativedelta
 
 _logger = logging.getLogger('ENVIO REPORTES')
 
@@ -12,22 +13,29 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     enviar_estado_cuenta = fields.Boolean('Enviar Estado de Cuenta', default=False)
+    planilla_estado_cuenta_id = fields.Many2one('mail.template', 'Planilla Estado de Cuenta')
+
     enviar_estado_pendiente = fields.Boolean('Enviar Estado de Pendientes', default=False)
+    planilla_estado_pendiente_id = fields.Many2one('mail.template', 'Planilla Estado Pendiente')
+
     email_alternativo = fields.Char('Email alternativo')
 
     statement_defaults = fields.Text('Statement defaults')
     outstanding_defaults = fields.Text('Outstanding defaults')
 
+    numero_veces = fields.Integer('Ejecutar cada', default=1)
+    periodo = fields.Selection([
+        ('manually', 'Manual'),
+        ('daily', 'Diario'),
+        ('weekly', 'Semanal'),
+        ('monthly', 'Mensual')
+    ], 'Periodo', default='monthly')
+    proximo_envio = fields.Date('Próximo envío')
+
     # Campos de control
     ultimo_envio = fields.Datetime('Último envío')
-    proximo_envio = fields.Datetime('Próximo envío', compute='compute_proximo_envio')
 
     only_company_ids = fields.Many2many('res.company', string='Solo enviar en compañías')
-
-    def compute_proximo_envio(self):
-        for rec in self:
-            cron_id = self.env.ref('partner_statement_auto_mail_queue.ir_cron_send_report')
-            rec.proximo_envio = cron_id.nextcall
 
     def get_base_config_json(self):
         self.ensure_one()
@@ -47,6 +55,12 @@ class ResPartner(models.Model):
 
             if rec.enviar_estado_pendiente and not rec.outstanding_defaults:
                 rec.outstanding_defaults = json.dumps(rec.get_base_config_json(), indent=4)
+
+            if rec.enviar_estado_cuenta:
+                rec.planilla_estado_cuenta_id = self.env.ref('partner_statement_auto_mail_queue.email_template_reporte_estados_cuenta', raise_if_not_found=False) or False
+
+            if rec.enviar_estado_pendiente:
+                rec.planilla_estado_pendiente_id = self.env.ref('partner_statement_auto_mail_queue.email_template_reporte_estados_pendiente', raise_if_not_found=False) or False
 
     def generar_pdf_estado_cuenta(self, company_id):
         self.ensure_one()
@@ -97,15 +111,12 @@ class ResPartner(models.Model):
             'mimetype': 'application/pdf',
         })
 
-    def enviar_correo(self, company_id, template_name):
+    def enviar_correo(self, company_id, template_id):
         for rec in self:
-            _logger.info('ENVIO REPORTE %s PARTNER %s', template_name, rec)
+            _logger.info('ENVIAR CORREO %s PARTNER %s', template_id, rec)
 
             try:
                 with self.env.cr.savepoint():
-
-                    template_id = self.env.ref(template_name)
-
                     record_values = template_id._generate_template(
                         rec.ids,
                         ['attachment_ids',
@@ -134,7 +145,7 @@ class ResPartner(models.Model):
 
                         mail_id = self.env['mail.mail'].sudo().create(mail_values)
 
-                        if template_name == 'partner_statement_auto_mail_queue.email_template_reporte_estados_cuenta':
+                        if rec.enviar_estado_cuenta:
                             attachemnts_ids = rec.generar_pdf_estado_cuenta(company_id)
                         else:
                             attachemnts_ids = rec.generar_pdf_estado_pendientes(company_id)
@@ -146,20 +157,55 @@ class ResPartner(models.Model):
             except Exception as e:
                 _logger.info('Error ENVIANDO REPORTE', e)
                 msg = f"Error enviando reporte: {str(e)}"
-                # rec.message_post(msg)
+                rec.message_post(msg)
 
             rec.write({
                 'ultimo_envio': fields.Datetime.now(),
             })
 
+    def avanzar_proximo_envio(self):
+        self.ensure_one()
+        next_update = False
+
+        if self.periodo == 'daily':
+            next_update = relativedelta(days=+self.numero_veces)
+
+        if self.periodo == 'weekly':
+            next_update = relativedelta(weeks=+self.numero_veces)
+
+        if self.periodo == 'monthly':
+            next_update = relativedelta(months=+self.numero_veces)
+
+        if not next_update:
+            self.write({'proximo_envio': False})
+        else:
+            self.write({'proximo_envio': date.today() + next_update})
+
     @api.model
     def cron_enviar_reportes(self):
-        partner_estado_cuenta_ids = self.env['res.partner'].search([('enviar_estado_cuenta', '=', True)])
-        partner_estado_pendiente_ids = self.env['res.partner'].search([('enviar_estado_pendiente', '=', True)])
+        today = fields.Date.today()
+
+        partner_ids = self.env['res.partner'].search([
+            ('enviar_estado_cuenta', '=', True),
+            ('proximo_envio', '<=', today),
+            ('periodo', '!=', 'manually'),
+        ])
+
+        partner_ids += self.env['res.partner'].search([
+            ('enviar_estado_pendiente', '=', True),
+            ('proximo_envio', '<=', today),
+            ('periodo', '!=', 'manually'),
+            ('id', 'not in', partner_ids.ids),
+        ])
 
         for company_id in self.env['res.company'].search([]):
-            filter_partner_ids = partner_estado_cuenta_ids.filtered(lambda l: (company_id in l.only_company_ids) or len(l.only_company_ids) == 0)
-            filter_partner_ids.enviar_correo(company_id, 'partner_statement_auto_mail_queue.email_template_reporte_estados_cuenta')
+            filter_partner_ids = partner_ids.filtered(lambda l: (company_id in l.only_company_ids) or len(l.only_company_ids) == 0)
 
-            filter_partner_ids = partner_estado_pendiente_ids.filtered(lambda l: (company_id in l.only_company_ids) or len(l.only_company_ids) == 0)
-            filter_partner_ids.enviar_correo(company_id, 'partner_statement_auto_mail_queue.email_template_reporte_estados_pendiente')
+            for partner_id in filter_partner_ids:
+                if partner_id.enviar_estado_cuenta:
+                    partner_id.enviar_correo(company_id, partner_id.planilla_estado_cuenta_id)
+
+                if partner_id.enviar_estado_pendiente:
+                    partner_id.enviar_correo(company_id, partner_id.planilla_estado_pendiente_id)
+
+                partner_id.avanzar_proximo_envio()
