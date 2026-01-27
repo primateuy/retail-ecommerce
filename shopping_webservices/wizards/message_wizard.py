12785c3e96427986c74826325d47b7feb81e5531
Alan Fránquez <santifranquez@gmail.com>
2026-01-27 15:11:51 -0300

    feat: url y password cambiados a diarios y lógica cambiada, se quitó res.config

--- shopping_webservices/wizards/message_wizard.py
+++ shopping_webservices/wizards/message_wizard.py
@@ -6,3 +6,7 @@ class MessageWizard(models.TransientModel):
     _description = 'Message Wizard'
 
     message = fields.Text(string="Message", readonly=True)
+
+
+    def action_close(self):
+        return {'type': 'ir.actions.act_window_close'}
