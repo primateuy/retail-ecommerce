89637d8c97f7bb2c85001a246118f997f3833ce4
Mario <mario.caro@estudiantes.utec.edu.uy>
2026-02-26 11:05:47 -0300

    fix: Correcciones del mail del dia 25/02/26

--- pos_mercadopago/__manifest__.py
+++ pos_mercadopago/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "POS MercadoPago Integration",
-    "version": "17.0.1.0.14",
+    "version": "17.0.1.0.19",
     "author": "Tu Nombre / Empresa",
     "category": "Point of Sale",
     "depends": ["base","payment_mercado_pago","pos_mercado_pago"],
