d04e82d7b19249acf8767d4fcc0c09b5f1d7a17d
iglesias-andres <aiglesias.giordano@gmail.com>
2026-03-05 11:21:57 -0300

    Revert "volviendo a commit anterior en fenicio"
    This reverts commit d8fe66e0ca7b774095b3d041a33d345e51113de5.
    
    :wq

--- odoo_fenicio/__manifest__.py
+++ odoo_fenicio/__manifest__.py
@@ -5,7 +5,7 @@
     'description': """INTEGRACION FENICIO""",
     'category': 'Localization',
     'version': '17.0.0.0',
-    'depends': ['base', 'sale', 'sale_management', 'account', 'stock', 'product_brand', 'website_sale', 'contacts', 'l10n_uy_einvoice_base', 'partner_contact_gender'],
+    'depends': ['base', 'sale', 'sale_management', 'account', 'stock', 'product_brand', 'website_sale', 'contacts', 'l10n_uy_einvoice_base', 'partner_contact_gender', 'loyalty'],
     'data': [
         'security/ir.model.access.csv',
         'views/product_attribute_views.xml',
@@ -22,7 +22,8 @@
         'data/data.xml',
         'data/payments_diaries.xml',
         'views/res_partner_view.xml',
-        'views/res_config.xml'
+        'views/res_config.xml',
+        'views/fenicio_log_views.xml',
     ],
     'license': 'LGPL-3',
 }
--- odoo_fenicio/models/stock_location.py
+++ odoo_fenicio/models/stock_location.py
@@ -8,3 +8,20 @@ class StockLocation(models.Model):
         default=False,
         help='Si está marcado, el stock de esta ubicación se enviará a Fenicio'
     )
+
+
+    def write(self, vals):
+        res = super(StockLocation, self).write(vals)
+        
+        if 'fenicio_visible' in vals and not vals['fenicio_visible']:
+            companies = self.env['res.company'].search([
+                ('fenicio_stock_location_ids', 'in', self.ids)
+            ])
+            if companies:
+                for location in self:
+                    companies.write({
+                        'fenicio_stock_location_ids': [(3, location.id)]
+                    })
+        
+        return res
+
