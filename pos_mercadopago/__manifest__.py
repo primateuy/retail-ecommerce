065220a50a0abc253814199b52cb5ffda9e57507
Mario <mario.caro@estudiantes.utec.edu.uy>
2026-03-17 10:39:59 -0300

    fix: Correcciones aplicadas al Testing MP 16/03/2026

--- pos_mercadopago/__manifest__.py
+++ pos_mercadopago/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "POS MercadoPago Integration",
-    "version": "17.0.1.0.21",
+    "version": "17.0.1.0.22",
     "author": "Tu Nombre / Empresa",
     "category": "Point of Sale",
     "depends": ["base","payment_mercado_pago","pos_mercado_pago"],
