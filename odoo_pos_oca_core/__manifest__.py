a4b1b054d1a81e4e04cfa34f2ac5c005e699dd95
iglesias-andres <aiglesias.giordano@gmail.com>
2026-06-04 15:48:40 -0300

    [IMP][4452] Ocultar botones Force Done y Cancelar en pantalla POS
      Oculta el bloque completo del estado 'waitingCard' (texto "Waiting for
      card" + botones Force Done y Cancel) reemplazándolo con un bloque vacío
      que preserva la cadena t-if/t-elif. También suprime el botón Force Done
      del estado 'force_done' con t-if=false.
    
      Cambio en: odoo_pos_oca_core/static/src/overrides/xml/payment_lines.xml
      Desarrollado por: Nicolás Calabria

--- odoo_pos_oca_core/__manifest__.py
+++ odoo_pos_oca_core/__manifest__.py
@@ -24,6 +24,7 @@ arrastra automáticamente desde ``odoo_pos_oca`` y ``odoo_pos_oca_backend``.
         "account",
         "payment",
         "account_payment",
+        "point_of_sale",
     ],
     "data": [
         "security/ir.model.access.csv",
@@ -31,6 +32,11 @@ arrastra automáticamente desde ``odoo_pos_oca`` y ``odoo_pos_oca_backend``.
         "views/payment_provider_views.xml",
         "views/payment_transaction_views.xml",
     ],
+    "assets": {
+        "point_of_sale._assets_pos": [
+            "odoo_pos_oca_core/static/src/overrides/xml/payment_lines.xml",
+        ],
+    },
     "pre_init_hook": "pre_init_hook",
     "post_init_hook": "post_init_hook",
     "license": "LGPL-3",
